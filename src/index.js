413c3e72e9f0580eddfdfad69078bd9e95432c5d
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2017-02-21 23:29:39 +0100

    pull secret from environment

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import startServer from './server'
 export default function () {
   const owner = 'dignifiedquire'
   const repo = 'realeaser'
-  const secret = 'a6d2987debf3cf4f5e59c2464c933ff47b6ba7301d21f516ea3826086ce6e49f'
+  const secret = process.env.GH_SECRET
+  if (!secret) {
+    throw new Error('Missing $GH_SECRET')
+  }
   const port = 5000
   const host = '0.0.0.0'
 
