27fee9bde8200895cee85f107c3b2ef59a080625
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2017-02-21 22:21:35 +0100

    fix server startup

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ export default function () {
   const owner = 'dignifiedquire'
   const repo = 'realeaser'
   const secret = 'a6d2987debf3cf4f5e59c2464c933ff47b6ba7301d21f516ea3826086ce6e49f'
-  const addr = '0.0.0.0:7777'
+  const port = 8888
+  const host = '0.0.0.0'
 
   const server = startServer(owner, repo, secret)
-  server.listen(addr)
+  server.listen(port, host)
 
-  console.log('Listening on %s/webhook', addr)
+  console.log('Listening on %s:%d/webhook', host, port)
 }
