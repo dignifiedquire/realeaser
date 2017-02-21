ccf9b111d664a6786e88e24afd6a79552f33b4ed
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2017-02-21 22:26:34 +0100

    change default port

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ export default function () {
   const owner = 'dignifiedquire'
   const repo = 'realeaser'
   const secret = 'a6d2987debf3cf4f5e59c2464c933ff47b6ba7301d21f516ea3826086ce6e49f'
-  const port = 8888
+  const port = 5000
   const host = '0.0.0.0'
 
   const server = startServer(owner, repo, secret)
