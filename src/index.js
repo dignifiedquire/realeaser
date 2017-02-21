d25083e6723c5bedc621b5e5d957b8ff0faff7d7
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2017-02-21 21:18:57 +0100

    first working version

---
+++ src/index.js
@@ -0,0 +1,77 @@
+/* @flow */
+'use strict'
+
+import GithubApi from 'github'
+import Listr from 'listr'
+
+const githubConfig = {
+  protocol: 'https',
+  Promise: Promise,
+  timeout: 5000
+}
+
+function checklist (version: string): string {
+  return `
+## Release v${version} :1st_place_medal:
+
+- [ ] first
+- [ ] second
+- [ ] third
+
+:tada: Benefit
+`.trim()
+}
+
+function fetchOauthToken (): string {
+  const token = process.env.GH_TOKEN || ''
+
+  if (token === '') {
+    throw new Error('Missing $GH_TOKEN to login with github')
+  }
+
+  return token
+}
+
+export default async function async () {
+  const github = new GithubApi(githubConfig)
+
+  const version = '1.3.0'
+  const owner = 'dignifiedquire'
+  const repo = 'realeaser'
+
+  const tasks = new Listr([{
+    title: 'Authenticate',
+    task (ctx) {
+      return github.authenticate({
+        type: 'oauth',
+        token: fetchOauthToken()
+      })
+    }
+  }, {
+    title: 'Create Milestone',
+    async task (ctx, task) {
+      const milestone = await github.issues.createMilestone({
+        owner,
+        repo,
+        title: version
+      })
+      console.log('Created milestone %s', milestone.data.html_url)
+      ctx.milestone = milestone.data
+      task.title = `Created ${milestone.data.html_url}`
+    }
+  }, {
+    title: 'Create Issue',
+    async task (ctx, task) {
+      const issue = await github.issues.create({
+        owner,
+        repo,
+        milestone: ctx.milestone.number,
+        title: `Checklist for v${version}`,
+        body: checklist(version)
+      })
+      task.title = `Created ${issue.data.html_url}`
+    }
+  }])
+
+  tasks.run()
+}
